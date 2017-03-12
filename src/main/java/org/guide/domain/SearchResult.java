71054b603ad7346432b27944db37aa08f7b02c42
MaryBeth <marybeth@rogerdavis.info>
2017-03-12 13:27:26 -0500

    added categories

--- src/main/java/org/guide/domain/SearchResult.java
+++ src/main/java/org/guide/domain/SearchResult.java
@@ -12,16 +12,22 @@ public class SearchResult {
     private String state;
     private String website;
     private String hours;
+    private String categoryId;
+    private String categoryName;
 
     public SearchResult(){}
 
-    public SearchResult(Service service) {
+    public SearchResult(Service service, Category category) {
         name = service.getAddress().getOrganization().getName();
         street = service.getAddress().getStreet();
         zipcode = service.getAddress().getZip();
         state = service.getAddress().getState();
         website = service.getAddress().getOrganization().getUrl();
         hours = service.getHours();
+        if (category != null){
+            categoryId = category.getId();
+            categoryName = category.getName();
+        }
     }
 
     public String getName() {
@@ -78,5 +84,21 @@ public class SearchResult {
 
     public void setHours(String hours) {
         this.hours = hours;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 }
